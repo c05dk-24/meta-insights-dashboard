5ff1ebeaad002df8f1e42a022e6cb51173b8ed7f
Dal Singh <dalsingh@Dals-Air.Home>
2024-12-08 01:20:15 +0000

    Your commit message

--- server/db/clear.sql
+++ server/db/clear.sql
@@ -1,4 +1,5 @@
 -- Drop all tables in the correct order to handle dependencies
+DROP TABLE IF EXISTS "CardLabels" CASCADE;
 DROP TABLE IF EXISTS "Comments" CASCADE;
 DROP TABLE IF EXISTS "Labels" CASCADE;
 DROP TABLE IF EXISTS "Cards" CASCADE;
@@ -6,8 +7,10 @@ DROP TABLE IF EXISTS "Lists" CASCADE;
 DROP TABLE IF EXISTS "Boards" CASCADE;
 DROP TABLE IF EXISTS "MetaInsights" CASCADE;
 DROP TABLE IF EXISTS "Users" CASCADE;
+DROP TABLE IF EXISTS "Companies" CASCADE;
 
 -- Drop any existing functions and triggers
+DROP TRIGGER IF EXISTS update_companies_updated_at ON "Companies";
 DROP TRIGGER IF EXISTS update_users_updated_at ON "Users";
 DROP TRIGGER IF EXISTS update_boards_updated_at ON "Boards";
 DROP TRIGGER IF EXISTS update_lists_updated_at ON "Lists";
