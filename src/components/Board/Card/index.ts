4c736aeec8a9688ebdabc0b7df1c1ae539bbfb40
Dal Singh <dalsingh@Mac.Home>
2024-12-15 16:31:42 +0000

    Your commit message

---
+++ src/components/Board/Card/index.ts
@@ -0,0 +1,15 @@
+import { CardModal } from './CardModal';
+import { CardDescription } from './CardDescription';
+import { CardChecklist } from './CardChecklist';
+import { CardComments } from './CardComments';
+import { CardDueDate } from './CardDueDate';
+import { CardLabels } from './CardLabels';
+
+export {
+  CardModal,
+  CardDescription,
+  CardChecklist,
+  CardComments,
+  CardDueDate,
+  CardLabels
+};
